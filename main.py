9e5775a181dfb339e298ef88282af284e028ef93
Tharusha <tharushadgunawardane@gmail.com>
2021-11-22 01:24:40 +0530

    commit delete user

--- main.py
+++ main.py
@@ -215,5 +215,15 @@ def settings():
                            )
 
 
+@app.route("/delete")
+def delete_user():
+    user = Users.query.filter_by(email="tharushadgunawardane@gmail.co").first()
+    if user:
+        db.session.delete(user)
+        db.session.commit()
+
+    return "Deleted user..."
+
+
 if __name__ == "__main__":
     app.run(debug=True)
